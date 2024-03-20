05dacbf6dd73984237401a637a2cdacca4ba8641
Olexiy Zyuzyukov <a.zyuzyukov@gmai.com>
2024-03-20 10:14:44 +0200

    sqlite for deploy

--- dgFramework/settings.py
+++ dgFramework/settings.py
@@ -77,14 +77,21 @@ WSGI_APPLICATION = "dgFramework.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.postgresql_psycopg2",
+#         "NAME": "dg_bae",
+#         "USER": "postgres",
+#         "PASSWORD": "postgres",
+#         "HOST": "localhost",
+#         "PORT": "5432"
+#     }
+# }
+
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.postgresql_psycopg2",
-        "NAME": "dg_bae",
-        "USER": "postgres",
-        "PASSWORD": "postgres",
-        "HOST": "localhost",
-        "PORT": "5432"
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
