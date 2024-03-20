58e9fac6105ac3589c751965023d3b49ae546f31
Olexiy Zyuzyukov <a.zyuzyukov@gmai.com>
2024-03-20 08:12:22 +0200

    First predeploy

--- dgFramework/urls.py
+++ dgFramework/urls.py
@@ -16,9 +16,13 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from .settings import DEBUG, MEDIA_ROOT, MEDIA_URL
+from django.conf.urls.static import static
 
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("storage/", include('storage.urls')),
     path("dm/", include('dm.urls')),
 ]
+
+if DEBUG:    urlpatterns += static(MEDIA_URL, document_root=MEDIA_ROOT)
